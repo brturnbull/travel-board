db6374065200801221902aa5750bd271995c7253
Bridget <brturnbul@gmail.com>
2018-09-14 13:32:54 +1000

    rehaul

--- src/app.js
+++ src/app.js
@@ -1,17 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import Home from './components/Home';
-
-class App extends React.Component {
-  render(){
-    return(
-      <Home />
-    );
-  }
-}
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+alert('hello');
