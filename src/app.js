364fcdaff06265c82b286396c426c0aafdfcffe2
Bridget <brturnbul@gmail.com>
2018-09-14 14:00:08 +1000

    react

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-alert('hello');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <h1>yo</h1>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
