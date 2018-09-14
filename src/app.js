be24961d0aea4645e6d1ab8b6913fec187e23b91
Bridget <brturnbul@gmail.com>
2018-09-14 11:10:15 +1000

    setup

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Home from './components/Home';
+
+class App extends React.Component {
+  render(){
+    return(
+      <Home />
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
